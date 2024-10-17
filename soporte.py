48ff535d23859be993ea77108fc356f43d5064f7
jcarreir <jav.racec@gmail.com>
2024-10-17 13:14:57 +0200

    Update

--- soporte.py
+++ soporte.py
@@ -30,7 +30,7 @@ def crear_df(tabla):
     df[0] = df[0].str.replace(' in', '')
     df[0] = df[0].str.replace(' %', '')
     df = df[0].str.split(' ', expand=True)
-    df.columns = datos = ["Date", "High Temp (ºF)", "Avg Temp (ºF)", "Low T (ºF)", "High Dew Pt (ºF)", "Avg Dew Pt (ºF)", "Low Dew Pt (ºF)", "High Hum (%)", "Avg Hum (%)", "Low Hum (%)", "High Spe (mph)", "Avg Spe (mph)", "Low Spe (mph)", "High Press (in)", "Low Press (in)", "Sum Prec (In)"]
+    df.columns = ["Date", "High Temp (ºF)", "Avg Temp (ºF)", "Low T (ºF)", "High Dew Pt (ºF)", "Avg Dew Pt (ºF)", "Low Dew Pt (ºF)", "High Hum (%)", "Avg Hum (%)", "Low Hum (%)", "High Spe (mph)", "Avg Spe (mph)", "Low Spe (mph)", "High Press (in)", "Low Press (in)", "Sum Prec (In)"]
     df.set_index('Date', inplace=True)
     df = df.applymap(lambda x: float(x))
     return df
